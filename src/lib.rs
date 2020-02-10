97cbaa1628ac8253e9bb450648b1be55f60c92cb
Gedalia Kott <gedkott@gmail.com>
2020-02-09 17:30:09 -0800

    track number of entries and buckets to compute load factor
    with load factor calculated, we can actually determine
    when to resize the hash table
    
    added test to show print statements that should accompany
    each load factor caculation and state when resizing should
    occur
    
    use 0.75 as load factor threshold because... apparently Java 10
    uses that threshold and I am not willing to find out alternatives
    right now

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,33 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 
 pub struct HashTable<K, V> {
     buckets: Vec<Vec<(K, V)>>,
+    total_entries: usize,
+    total_buckets: usize,
 }
 
 impl<K: std::hash::Hash + std::fmt::Debug + Clone + PartialEq, V: Clone + std::fmt::Debug>
     HashTable<K, V>
 {
     pub fn new() -> HashTable<K, V> {
+        let default_number_of_starting_buckets = 10;
         HashTable {
-            buckets: vec![vec![]; 10],
+            buckets: vec![vec![]; default_number_of_starting_buckets],
+            total_entries: 0,
+            total_buckets: default_number_of_starting_buckets,
         }
     }
 
     pub fn with_capacity(capacity: usize) -> HashTable<K, V> {
         HashTable {
             buckets: vec![vec![]; capacity],
+            total_entries: 0,
+            total_buckets: capacity,
         }
+    }
+
+    fn load_factor(&self) -> f64 {
+        // println!("te: {}, tb: {}, lf: {}", self.total_entries, self.total_buckets, self.total_entries as f64 / self.total_buckets as f64);
+        self.total_entries as f64 / self.total_buckets as f64
     }
 
     fn get_bucket_index(&self, k: &K) -> usize {
@@ -35,6 +47,12 @@ impl<K: std::hash::Hash + std::fmt::Debug + Clone + PartialEq, V: Clone + std::f
     pub fn insert(&mut self, k: K, v: V) -> () {
         let bucket_index = self.get_bucket_index(&k);
         self.buckets[bucket_index].push((k, v));
+        self.total_entries += 1;
+        let current_load_factor = self.load_factor();
+        println!("current load factor of {}", current_load_factor);
+        if current_load_factor > 0.75 {
+            println!("load factor threshold reached; should resize");
+        }
     }
 
     pub fn get(&self, k: K) -> Option<&V> {
@@ -114,5 +132,62 @@ mod tests {
 
         assert_eq!(gedalia_result, expected_gedalia_result);
         assert_eq!(theo_result, expected_theo_result);
+    }
+
+    #[test]
+    fn test_load_factor_threshold_calculation() {
+        let mut hash_table = HashTable::with_capacity(9);
+
+        let users = vec![
+            User {
+                name: "gedalia".to_string(),
+                age: 27,
+            },
+            User {
+                name: "theo".to_string(),
+                age: 0,
+            },
+            User {
+                name: "aviva".to_string(),
+                age: 26,
+            },
+            User {
+                name: "chani".to_string(),
+                age: 25,
+            },
+            User {
+                name: "nachmi".to_string(),
+                age: 24,
+            },
+            User {
+                name: "avery".to_string(),
+                age: 23,
+            },
+            User {
+                name: "caine".to_string(),
+                age: 22,
+            },
+        ];
+
+        for user in &users {
+            hash_table.insert(user.name.to_string(), user);
+        }
+
+        let gedalia_result = hash_table.get("gedalia".to_string());
+        let caine_result = hash_table.get("caine".to_string());
+
+        let gedalia = &User {
+            name: "gedalia".to_string(),
+            age: 27,
+        };
+        let expected_gedalia_result = Some(&gedalia);
+        let caine = &User {
+            name: "caine".to_string(),
+            age: 22,
+        };
+        let expected_caine_result = Some(&caine);
+
+        assert_eq!(gedalia_result, expected_gedalia_result);
+        assert_eq!(caine_result, expected_caine_result);
     }
 }
